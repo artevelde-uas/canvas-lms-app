db49f0cafeed298ce8fc819c1b791fad32393518
Renaat De Muynck <renaat.demuynck@gmail.com>
2018-12-11 15:27:12 +0100

    Add more routes

--- src/router.js
+++ src/router.js
@@ -13,7 +13,11 @@ const routes = {
     'courses.announcements': new Route('/courses/:courseId/announcements'),
     'courses.announcements.new': new Route('/courses/:courseId/discussion_topics/new?is_announcement=true'),
     'courses.discussions': new Route('/courses/:courseId/discussion_topics'),
+    'courses.discussions.new': new Route('/courses/:courseId/discussion_topics/new'),
+    'courses.discussions.view': new Route('/courses/:courseId/discussion_topics/:topicId'),
     'courses.gradebook': new Route('/courses/:courseId/gradebook'),
+    'courses.gradebook.history': new Route('/courses/:courseId/gradebook/history'),
+    'courses.gradebook.upload': new Route('/courses/:courseId/gradebook_upload/new'),
     'courses.grades': new Route('/courses/:courseId/grades'),
     'courses.outcomes': new Route('/courses/:courseId/outcomes'),
     'courses.quizzes': new Route('/courses/:courseId/quizzes'),
@@ -24,10 +28,13 @@ const routes = {
     'courses.pages.revisions': new Route('/courses/:courseId/pages/:pageSlug/revisions'),
     'courses.modules': new Route('/courses/:courseId/modules'),
     'courses.syllabus': new Route('/courses/:courseId/assignments/syllabus'),
+    'courses.files': new Route('/courses/:courseId/files'),
     'courses.assignments': new Route('/courses/:courseId/assignments'),
+    'courses.assignments.new': new Route('/courses/:courseId/assignments/new'),
     'courses.conferences': new Route('/courses/:courseId/conferences'),
     'courses.collaborations': new Route('/courses/:courseId/lti_collaborations'),
     'courses.users': new Route('/courses/:courseId/users'),
+    'courses.users.view': new Route('/courses/:courseId/users/:userId'),
     'courses.settings': new Route('/courses/:courseId/settings'),
     'courses.settings.statistics': new Route('/courses/:courseId/statistics'),
     'courses.settings.confirm-conclude': new Route('/courses/:courseId/confirm_action?event=conclude'),
@@ -35,12 +42,38 @@ const routes = {
     'courses.settings.copy': new Route('/courses/:courseId/copy'),
     'courses.settings.import': new Route('/courses/:courseId/content_migrations'),
     'courses.settings.export': new Route('/courses/:courseId/content_export'),
+    'courses.settings.undelete': new Route('/courses/:courseId/undelete'),
     'courses.external-tool': new Route('/courses/:courseId/external_tools/:toolId'),
-    
-    'profile.home': new Route('/profile'),
+
+    'groups': new Route('/groups'),
+    'groups.home': new Route('/groups/:groupId'),
+    'groups.announcements': new Route('/groups/:groupId/announcements'),
+    'groups.announcements.new': new Route('/groups/:groupId/announcements/discussion_topics/new?is_announcement=true'),
+    'groups.pages': new Route('/groups/:groupId/pages'),
+    'groups.pages.home': new Route('/groups/:groupId/wiki'),
+    'groups.pages.view': new Route('/groups/:groupId/pages/:pageSlug'),
+    'groups.pages.edit': new Route('/groups/:groupId/pages/:pageSlug/edit'),
+    'groups.users': new Route('/groups/:groupId/users'),
+    'groups.users.view': new Route('/groups/:groupId/users/:userId'),
+    'groups.discussions': new Route('/groups/:groupId/discussion_topics'),
+    'groups.discussions.new': new Route('/groups/:groupId/discussion_topics/new'),
+    'groups.discussions.view': new Route('/groups/:groupId/discussion_topics/:topicId'),
+    'groups.files': new Route('/groups/:groupId/files'),
+    'groups.conferences': new Route('/groups/:groupId/conferences'),
+    'groups.collaborations': new Route('/groups/:groupId/collaborations'),
+
+    'profile': new Route('/profile'),
+    'profile.about': new Route('/about[/:userId]'),
     'profile.communication': new Route('/profile/communication'),
     'profile.settings': new Route('/profile/settings'),
-    'profile.files': new Route('/files')
+    'profile.files': new Route('/files'),
+    'profile.eportfolios': new Route('/dashboard/eportfolios'),
+    'profile.eportfolios.home': new Route('/eportfolios/:portfolioId'),
+    'profile.eportfolios.view': new Route('/eportfolios/:portfolioId/:sectionSlug[/:pageSlug]'),
+
+    'calendar': new Route('/calendar'),
+
+    'inbox': new Route('/conversations')
 };
 
 
