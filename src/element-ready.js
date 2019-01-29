c39cf33ef112cb2c12ac3a8b3b160153bbfda579
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-01-29 09:42:02 +0100

    Add optional root argument

--- src/element-ready.js
+++ src/element-ready.js
@@ -1,30 +1,39 @@
 
 
-export default function (selector, handler) {
+export default function (root, selector, handler) {
     var promise = new Promise(function (resolve) {
-        var element = document.querySelector(selector);
-        var observer = new MutationObserver(function (mutation) {
+        var element, observer;
+        
+        if (typeof root === 'string' && typeof selector === 'function') {
+            handler = selector;
+            selector = root;
+            root = document.body;
+        }
+        
+        element = document.querySelector(selector);
+        
+        if (element !== null) {
+            return resolve(element);
+        }
+        
+        observer = new MutationObserver(function (mutation) {
             var nodes = Array.from(mutation.addedNodes || []);
-
+            
             for (let node of nodes) {
                 if (node instanceof Element && node.matches(selector)) {
                     observer.disconnect();
-
+                    
                     return resolve(node);
                 }
             }
         });
-
-        if (element !== null) {
-            return resolve(element);
-        }
-
-        observer.observe(document.body, {
+        
+        observer.observe(root, {
             childList: true,
             subtree: true
         });
     });
-
+    
     promise.then(handler, function (event) {
         console.log(event.message);
     }).catch(function (ex) {
