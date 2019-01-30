2a44b19cc84a2b76bc82aba7cf327ae9501a00e9
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-01-30 22:26:09 +0100

    fix: Remove stray semicolon

--- src/i18n.js
+++ src/i18n.js
@@ -4,7 +4,7 @@ import i18next from 'i18next';
 function setTranslations(translations) {
     let resources = {};
     
-    Object.entries(translations).forEach(function ([key, value]) {;
+    Object.entries(translations).forEach(function ([key, value]) {
         resources[key] = { 'translation': value };
     });
     
