40131eef7264b58085a24845d29905ec5625e9e4
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-07-08 08:21:56 +0200

    fix: Handle optional selector argument when root argument is omitted

--- src/element-ready.js
+++ src/element-ready.js
@@ -4,7 +4,7 @@ export default function (root, selector, handler) {
     var promise = new Promise(function (resolve) {
         var element, observer;
 
-        if (typeof root === 'string' && typeof selector === 'function') {
+        if (typeof root === 'string' && (typeof selector === 'function' || selector === undefined)) {
             handler = selector;
             selector = root;
             root = document.body;
