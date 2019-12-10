f5491c90d6926e35c7e6fb125927d8f6f6c86952
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2019-12-10 15:08:44 +0100

    Extract links from response header

--- src/api/CanvasApiResponse.js
+++ src/api/CanvasApiResponse.js
@@ -5,6 +5,25 @@
  */
 export default class CanvasApiResponse extends Response {
 
+    get links() {
+        let linkHeader = this.headers.get('Link');
+
+        if (linkHeader === null) return null;
+
+        return linkHeader.split(',').reduce((links, value) => {
+            var match = value.match(/^<https:\/\/.+\/api\/v1\/(.+)>; rel="(\w+)"$/);
+
+            if (match !== null) {
+                let type = match[2];
+                let url = '/' + match[1];
+
+                links[type] = url;
+            }
+
+            return links;
+        }, {});
+    }
+
     constructor(response) {
         super(response.body, {
             status: response.status,
