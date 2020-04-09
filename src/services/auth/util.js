5288ff27eda92b9fd12ba83561a7490b6d7f6823
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-04-09 22:44:34 +0200

    Cache the enrollment types

--- src/services/auth/util.js
+++ src/services/auth/util.js
@@ -2,6 +2,8 @@ import api from '@artevelde-uas/canvas-lms-api';
 import { routeMatch } from '../router';
 
 
+let enrollmentTypes;
+
 export async function hasCourseEnrollment(enrollmentType) {
     let path = window.location.pathname + window.location.search;
     let { params } = routeMatch(path);
@@ -10,10 +12,13 @@ export async function hasCourseEnrollment(enrollmentType) {
         throw new Error('Function called out of course context');
     }
 
-    let url = `/courses/${params.courseId}/enrollments`;
-    let enrollments = await api.get(url, { user_id: 'self' });
+    // Cache the enrollment types
+    if (enrollmentTypes === undefined) {
+        let url = `/courses/${params.courseId}/enrollments`;
+        let enrollments = await api.get(url, { user_id: 'self' });
 
-    console.log(enrollments);
+        enrollmentTypes = new Set(enrollments.map(({ type }) => type));
+    }
 
-    return enrollments.some(({ type }) => (type === enrollmentType));
+    return enrollmentTypes.has(enrollmentType);
 }
