a17b287b7171a123f5672afbcfe54f653b85a958
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-04-10 00:28:40 +0200

    Use map instead of array

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,15 @@
 
-var services = [];
+const services = new Map();
 
 
 export function addService(name, service) {
-    services.push({ name, service });
+    services.set(name, service);
 }
 
 export function createServiceManager() {
     const serviceManager = {};
 
-    services.forEach(function ({ name, service }) {
+    services.forEach((service, name) => {
         serviceManager[name] = (typeof service === 'function') ? service(serviceManager) : service;
     });
 
