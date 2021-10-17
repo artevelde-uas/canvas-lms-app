501d4461c1f5df4ce05d10da83e3c3de21a24a2c
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-10-17 14:17:38 +0200

    Add React support

--- src/services/i18n/index.js
+++ src/services/i18n/index.js
@@ -1,8 +1,10 @@
 import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 
 const language = document.documentElement.lang || window.navigator.language;
 
+i18n.use(initReactI18next);
 i18n.init({
     lng: language,
     fallbackLng: 'en',
