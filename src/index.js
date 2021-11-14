4631f2909e4d54e2f4f85d0aac691aad8ec93869
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-11-14 15:34:20 +0100

    Add more comments

--- src/index.js
+++ src/index.js
@@ -24,6 +24,13 @@ addService('addReadyListener', () => elementReady); // DEPRECATED: use `dom.onEl
 const plugins = new Map();
 
 
+/**
+ * Registers a plug-in
+ * 
+ * @param {function} plugin The plugin to register
+ * @param {object} options The options to pass to the plugin
+ * @param {boolean} options.classicPlugin Indicates if the plug-in is a classic one that needs the services injected
+ */
 function addPlugin(plugin, options = { classicPlugin: false }) {
     var serviceManager = createServiceManager();
 
@@ -33,10 +40,12 @@ function addPlugin(plugin, options = { classicPlugin: false }) {
         try {
             switch (typeof plugin) {
                 case 'function':
+                    // Run the plugin with services and the provided options
                     plugin(serviceManager, options);
                     break;
 
                 case 'object':
+                    // DEPRECATED: Invoke init method of object plug-ins
                     plugin.init(serviceManager, options);
                     break;
             }
@@ -48,19 +57,27 @@ function addPlugin(plugin, options = { classicPlugin: false }) {
     }
 
     try {
+        // Run the plugin with the provided options
         const info = plugin(options);
 
+        // Store the plugin data
         plugins.set(plugin, { info, options });
     } catch (err) {
         console.error(err.toString());
     }
 }
 
+/**
+ * Starts the application and runs each registered plug-in
+ */
 function run() {
     var path = window.location.pathname + window.location.search;
+    // Get the current path with the query string
 
+    // Don't run inside iframes
     if (window !== window.top) return;
 
+    // Let the router handle the current path
     handlePath(path);
 }
 
