f1b55c77f35c57a28e846206de9369797dfe20b4
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-11-16 18:40:36 +0100

    Show the list of current installed plug-ins on the settings profile page

---
+++ src/plugins/plugins-list/components/PluginTable.jsx
@@ -0,0 +1,54 @@
+import { Table } from '@instructure/ui-table';
+import { ToggleDetails } from '@instructure/ui-toggle-details'
+import { Text } from '@instructure/ui-text'
+import { IconButton } from '@instructure/ui-buttons'
+import { IconPublishSolid } from '@instructure/ui-icons'
+
+import { EmotionThemeProvider } from '@instructure/emotion';
+import { theme } from '@artevelde-uas/canvas-lms-app';
+
+
+export default ({ plugins }) => (
+    <EmotionThemeProvider theme={theme}>
+        <Table caption='Plugin list'>
+            <Table.Head>
+                <Table.Row>
+                    <Table.ColHeader id='plugin' width='100%'>
+                        Plug-in
+                    </Table.ColHeader>
+                    <Table.ColHeader id='status'>
+                        Status
+                    </Table.ColHeader>
+                </Table.Row>
+            </Table.Head>
+            <Table.Body>
+                {plugins.map((plugin) => {
+                    const title = plugin.title || plugin.name;
+
+                    return (
+                        <Table.Row key={plugin.name}>
+                            <Table.Cell id='plugin'>
+                                <ToggleDetails
+                                    summary={title}
+                                >
+                                    <Text>{plugin.description}</Text>
+                                </ToggleDetails>
+                            </Table.Cell>
+                            <Table.Cell id='status'>
+                                <IconButton
+                                    color='success'
+                                    withBorder={false}
+                                    withBackground={false}
+                                    interaction='readonly'
+                                    screenReaderLabel={title}
+                                >
+                                    <IconPublishSolid />
+                                </IconButton>
+                            </Table.Cell>
+                        </Table.Row>
+                    );
+                })}
+            </Table.Body>
+        </Table>
+    </EmotionThemeProvider>
+);
