0e442ec31468d9a1279fa37447f55e2c9c7c5df1
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-12-16 16:58:03 +0100

    fix: also transpile files inside 'node_modules'

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -13,7 +13,6 @@ module.exports = {
     module: {
         rules: [{
             test: /\.jsx?$/,
-            exclude: /node_modules/,
             use: [{
                 loader: 'babel-loader',
                 options: {
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -15,7 +15,6 @@ module.exports = {
     module: {
         rules: [{
             test: /\.jsx?$/,
-            exclude: /node_modules/,
             use: [{
                 loader: 'babel-loader',
                 options: {
