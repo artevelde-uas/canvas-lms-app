085ca336003e1f4c9121e2eb21303dd1938c6715
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-20 22:23:44 +0100

    Check for existence of paths and use HereDoc style ES6 template tag

--- src/server/start.js
+++ src/server/start.js
@@ -1,35 +1,101 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const heredoc = require('heredoc-tag').default.unindent.trim;
 
 const appRoot = process.cwd();
+const distFolder = path.join(appRoot, '/dist');
 const package = require(path.join(appRoot, '/package.json'));
 const port = package.config?.server?.port || 5000;
 const serveFile = package.config?.server?.file || 'desktop.dev.js';
 
 
+console.log('\n');
+
+// Create 'dist' folder if it doesn't exist
+if (!fs.existsSync(distFolder)) {
+    console.log(heredoc`
+        Folder '${distFolder}' does not exist.
+         -> Creating '${distFolder}' ...
+    ` + '\n');
+
+    fs.mkdirSync(distFolder);
+
+    // Check for compiled script
+    if (!fs.existsSync(serveFile)) {
+        console.log(heredoc`
+            WARNING: File '${serveFile}' does not exist.
+                     Please compile your source files before serving.
+        `, '\n');
+    }
+}
+
 // Concatenate the CSS warning files and output to 'dist' folder
-{
+themeWarnings: {
+    const hideThemeWarningsStylesFile = path.join(__dirname, '/hide-theme-editor-warning.css');
+    const hideEnvWarningsStylesFile = path.join(__dirname, '/hide-test-env-warning.css');
+    const hideWarningsStylesPath = path.join(distFolder, '/hide-warnings.css');
+
     const hideWarningsStyles = (
-        fs.readFileSync(path.join(__dirname, '/hide-theme-editor-warning.css'), 'utf8') + '\n' +
-        fs.readFileSync(path.join(__dirname, '/hide-test-env-warning.css'), 'utf8')
+        fs.readFileSync(hideThemeWarningsStylesFile, 'utf8') + '\n' +
+        fs.readFileSync(hideEnvWarningsStylesFile, 'utf8')
     );
 
-    fs.writeFileSync(path.join(appRoot, '/dist/hide-warnings.css'), hideWarningsStyles);
+    if (!fs.existsSync(hideWarningsStylesPath)) {
+        console.log(heredoc`
+            File '${hideWarningsStylesPath}' does not exist.
+             -> Creating '${hideWarningsStylesPath}' ...
+                Please upload this CSS file to your Canvas theme.
+        `, '\n');
+    } else {
+        const currentHideWarningsStyles = fs.readFileSync(hideWarningsStylesPath, 'utf8');
+
+        if (currentHideWarningsStyles === hideWarningsStyles) {
+            break themeWarnings;
+        }
+
+        console.log(heredoc`
+            File '${hideWarningsStylesPath}' was modified.
+              Please upload this CSS file to your Canvas theme again.
+        `, '\n');
+    }
+
+    fs.writeFileSync(hideWarningsStylesPath, hideWarningsStyles);
 }
 
 // Replace URL inside the server file and output to 'dist' folder
-{
-    let serveLocalScript = fs.readFileSync(path.join(__dirname, '/serve-local.js.dist'), 'utf8');
+serveLocal: {
+    const serveLocalScriptFile = path.join(distFolder, '/serve-local.js');
+    const serveLocalScriptDistFile = path.join(__dirname, '/serve-local.js.dist');
 
-    serveLocalScript = serveLocalScript.replace(/{{URL}}/, `http://127.0.0.1:${port}`)
+    const serveLocalScriptDist = fs.readFileSync(serveLocalScriptDistFile, 'utf8');
+    const serveLocalScript = serveLocalScriptDist.replace(/{{URL}}/, `http://127.0.0.1:${port}`)
 
-    fs.writeFileSync(path.join(appRoot, '/dist/serve-local.js'), serveLocalScript);
+    if (!fs.existsSync(serveLocalScriptFile)) {
+        console.log(heredoc`
+            File '${serveLocalScriptFile}' does not exist.
+             -> Creating '${serveLocalScriptFile}' ...
+                Please upload this JavaScript file to your Canvas theme.
+        `, '\n');
+    } else {
+        const currentServeLocalScript = fs.readFileSync(serveLocalScriptFile, 'utf8');
+
+        if (currentServeLocalScript === serveLocalScript) {
+            break serveLocal;
+        }
+
+        console.log(heredoc`
+            File '${serveLocalScriptFile}' was modified.
+              Please upload this JavaScript file to your Canvas theme again.
+        `, '\n');
+    }
+
+    fs.writeFileSync(serveLocalScriptFile, serveLocalScript);
 }
 
 // Create an instance of the http server to handle HTTP requests
 const server = http.createServer((request, response) => {
-    const filePath = path.join(appRoot, '/dist/', serveFile);
+    const filePath = path.join(distFolder, '/', serveFile);
     const stat = fs.statSync(filePath);
 
     response.writeHead(200, {
@@ -45,4 +111,4 @@ const server = http.createServer((request, response) => {
 // Start the server on port 3000
 server.listen(port, '127.0.0.1');
 
-console.log(`Serving '/dist/${serveFile}' on 'http://127.0.0.1:${port}'`);
+console.log(`Serving '/dist/${serveFile}' on 'http://127.0.0.1:${port}'`, '\n');
