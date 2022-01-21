b93f6adac056496ae2dbab80ab08cee3ef95012c
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-21 13:40:20 +0100

    Use loop instead of recursion

--- src/server/util.js
+++ src/server/util.js
@@ -1,17 +1,15 @@
 
-function log(text, ...args) {
-    const lines = text.replace(/(^\s*\n|\n\s*$)/g, '').split(/\n/);
-    const indentSize = lines.reduce((minimumIndentSize, currentLine) => {
-        const lineIndentSize = currentLine.search(/[^ ]|$/);
-
-        return Math.min(minimumIndentSize, lineIndentSize);
-    }, Number.POSITIVE_INFINITY);
-
-    console.log(lines.map(line => line.slice(indentSize)).join('\n'));
-
-    if (args.length > 0) {
-        log(...args);
-    }
+function log(...args) {
+    args.forEach(text => {
+        const lines = text.replace(/(^\s*\n|\n\s*$)/g, '').split(/\n/);
+        const indentSize = lines.reduce((minimumIndentSize, currentLine) => {
+            const lineIndentSize = currentLine.search(/[^ ]|$/);
+            
+            return Math.min(minimumIndentSize, lineIndentSize);
+        }, Number.POSITIVE_INFINITY);
+        
+        console.log(lines.map(line => line.slice(indentSize)).join('\n'));
+    });
 }
 
 exports.log = log;
