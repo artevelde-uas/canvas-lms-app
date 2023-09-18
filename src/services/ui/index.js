0a34a3d798a5c760601037e1e2054a24cf4ca010
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-09-18 13:31:01 +0200

    Add default title

--- src/services/ui/index.js
+++ src/services/ui/index.js
@@ -2,15 +2,18 @@
 export function createInfoButton(contents, options = {}) {
     const infoButton = document.createElement('button');
     const infoIcon = document.createElement('i');
+    const title = options.title ?? 'More info';
 
     infoButton.className = 'Button Button--icon-action';
     infoIcon.className = 'icon-question';
+    infoIcon.title = title;
     infoButton.append(infoIcon);
 
     const dialog = jQuery(contents).dialog({
         ...options,
         autoOpen: false,
-        modal: true
+        modal: true,
+        title
     });
 
     infoButton.addEventListener('click', event => {
