46cda6b7c59836acf8fa7d9be9cbde24a93b533c
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-09-18 13:30:34 +0200

    Add `createInfoButton` method

--- src/services/ui/index.js
+++ src/services/ui/index.js
@@ -1,2 +1,25 @@
 
-export default {};
+export function createInfoButton(contents, options = {}) {
+    const infoButton = document.createElement('button');
+    const infoIcon = document.createElement('i');
+
+    infoButton.className = 'Button Button--icon-action';
+    infoIcon.className = 'icon-question';
+    infoButton.append(infoIcon);
+
+    const dialog = jQuery(contents).dialog({
+        ...options,
+        autoOpen: false,
+        modal: true
+    });
+
+    infoButton.addEventListener('click', event => {
+        dialog.dialog('open');
+    });
+
+    return infoButton;
+}
+
+export default {
+    createInfoButton
+};
